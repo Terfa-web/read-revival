ab456f161c23c1d05dd174707fca31d099670c79
Ter-web <n.terfa@yahoo.com>
2024-01-04 14:04:57 +0200

    authenticate user login details

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,24 @@
+import { register } from "@/lib/actions";
+import styles from "./register.module.css";
+
+const RegisterPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action={register}>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,9 +1,10 @@
 "use server";
 
 import { signIn, signOut } from "./auth";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { revalidatePath } from "next/cache";
+import bcrypt from 'bcryptjs';
 
 
 export const addPost = async (formData) => {  
@@ -23,7 +24,7 @@ try {
   console.log("saved to db")
   // revalidatePath("/blog");
 } catch (error) {
-    console.log(error)
+    console.log(error.message)
     return{error: "something went wrong"}
 }
 }
@@ -53,3 +54,50 @@ export const handleLogout = async () => {
   "use server";
   await signOut();
 };
+
+export const register = async (formData) => {
+  const {username, email, password, img, passwordRepeat} = Object.fromEntries(formData);
+
+  if(password !== passwordRepeat) {
+    return "password doesn't match"
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({username});
+
+    if(user){
+      return "User already exists"
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved newuser to db")
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went wrong!"}
+  }
+}
+
+
+export const login = async (formData) => {
+  const {username, password} = Object.fromEntries(formData);
+
+
+  try {
+    await signIn("credentials", { username, password}); 
+    console.log("logged in")   
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went wrong!"}
+  }
+}
