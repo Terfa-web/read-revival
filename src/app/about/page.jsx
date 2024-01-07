98a99f47e97d963bbbf1bfc9a6c1a3c88507ab12
Ter-web <n.terfa@yahoo.com>
2024-01-07 20:04:11 +0200

    add final animations

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,11 +8,11 @@ export const metadata = {
 
 const AboutPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.firstContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>Mission Statement</h1>
-          <p className={styles.text}>
+    <div className={`${styles.container} easeIn`}>
+      <div className={`${styles.firstContainer} easeUp`}>
+        <div className={`${styles.textContainer} fadeOut`}>
+          <h1 className={`${styles.title} easeIn`}>Mission Statement</h1>
+          <p className={`${styles.text} fadeIn`}>
             To sorce for and make available to students a variety of
             supplementary To source for and make available to students a variety
             of supplementary readers and other books, and to corroborate with
@@ -20,9 +20,9 @@ const AboutPage = () => {
             information, knowledge, and enjoyment.
           </p>
         </div>
-        <div className={styles.imgContainer}>
+        <div className={`${styles.imgContainer} easeDown active`}>
           <Image
-            className={styles.imgContainerImage}
+            className={`${styles.imgContainerImage}`}
             src="/handingbooktostudents.jpg"
             alt=""
             fill
@@ -30,9 +30,9 @@ const AboutPage = () => {
           />
         </div>
       </div>
-      <div className={styles.secondContainer}>
-        <h1 className={styles.title}>The RR Initiative</h1>
-        <p className={styles.text}>
+      <div className={`${styles.secondContainer} easeIn active`}>
+        <h1 className={`${styles.title} fadeIn`}>The RR Initiative</h1>
+        <p className={`${styles.text} easeUp`}>
           The main aim of this INITIATIVE is to stimulate students to engage in
           reading books and developing their reading culture. To achieve this
           goal, the INITIATIVE will embark on procuring books and making them
@@ -46,20 +46,20 @@ const AboutPage = () => {
           be encouraged to allocate at least one period of English Language for
           reading and accompanying exercises.
         </p>
-        <div className={styles.img2Container}>
+        <div className={`${styles.img2Container} fadeOut`}>
           <Image
-            className={styles.imgContainerImage}
+            className={`${styles.imgContainerImage}`}
             src="/group1.jpg"
             alt="group pic"
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
-        <h1 className={styles.title2}>
+        <h1 className={`${styles.title2} easeIn`}>
           {" "}
           Criteria for a school to receive books
         </h1>
-        <ul className={styles.text2}>
+        <ul className={`${styles.text2} fadeIn`}>
           <li>The School must have a functional library</li>
           <li>
             There must be a qualified teacher of English Language to handle the
@@ -68,14 +68,16 @@ const AboutPage = () => {
           <li>The school must be government approved</li>
           <li>Willingness by the school heads to participate in the program</li>
         </ul>
-        <h1 className={styles.title3}>How do we measure our success?</h1>
-        <p className={styles.text3}>
+        <h1 className={`${styles.title3} easeDown active`}>
+          How do we measure our success?
+        </h1>
+        <p className={`${styles.text3} easeUp active`}>
           Each student participating in the initiative will be encouraged to
           always fill out a simple questionnaire in their “Yes, I can Read
           Booklet”. These questions should be answered after each book is read;
         </p>
       </div>
-      <ol className={styles.text3}>
+      <ol className={`${styles.text3} ${styles.fadeOut}`}>
         <li>What is the title of the book?</li>
         <li>What is the name of the Author?</li>
         <li>What is the story about?</li>
@@ -87,10 +89,10 @@ const AboutPage = () => {
         <li>What is the moral message the book is giving to us?</li>
         <li>In which environment is the story set?</li>
       </ol>
-      <h1 className={styles.title4}>
+      <h1 className={`${styles.title4} easeIn`}>
         How do we reward schools performing well?
       </h1>
-      <ol className={styles.text4}>
+      <ol className={`${styles.text4} fadeIn`}>
         <li>
           Awarding books/cash incentives to participating schools and teachers
         </li>
